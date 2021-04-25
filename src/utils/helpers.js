f6cc2acb1fbb5daf55068fbabe169f9f78f9b10a
Rein <windsailg@gmail.com>
2021-04-25 16:49:16 +0800

    change api

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 let baseURL = ''
-baseURL = 'http://localhost:3000'
-// baseURL = 'https://meeting-room-api.herokuapp.com'
+// baseURL = 'http://localhost:3000'
+baseURL = 'https://meeting-room-api.herokuapp.com'
 
 export const apiHelper = axios.create({
   baseURL
